a5fd88d82c491b685d41dd0ba6d9968b81e81583
Rencheng Chua <rench00@gmail.com>
2016-08-09 15:06:03 +0800

    update html and js file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(function(){
   $button = $('#quote-now');  // this is the button
-  $result = $('#result'); // this selects the result <p>
+  $content = $('#quote-content'); // this selects the result <p>
   api_url = "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous"; // this is the url for the api
   $button.on('click', function(){
     $.ajax({
@@ -15,7 +15,7 @@ $(function(){
   }
 
   function successFunction(data){
-    // put the action to show only the quote in the $result
+    // put the action to show the quote and the person who said the quote in the $result
     // we can have this do some changes to the styling to (eg make the container for the result appear only when success)
   }
 
