67a57a3ec6b32711272f0efc7ff3e2b5c12b8ac2
Rencheng Chua <rench00@gmail.com>
2016-08-09 14:48:20 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+$(function(){
+  $button = $('#quote-now');  // this is the button
+  $result = $('#result'); // this selects the result <p>
+  api_url = "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous"; // this is the url for the api
+  $button.on('click', function(){
+    $.ajax({
+      // put the necessary code here to hit the api
+    }).always(alwaysFunction)
+      .success(successFunction)
+      .fail(failFunction);
+
+  function alwaysFunction(){
+    console.log("yay!");
+    // do we need this? if not take it out
+  }
+
+  function successFunction(data){
+    // put the action to show only the quote in the $result
+    // we can have this do some changes to the styling to (eg make the container for the result appear only when success)
+  }
+
+  function failFunction(jqXHR, textStatus, errorThrown){
+    console.log(errorThrown);
+  }
+  })
+})
