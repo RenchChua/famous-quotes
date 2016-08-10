a84b56234557b8e5d47d71b667058bbdf3ee415a
Sebastian Sim <phistando@yahoo.com>
2016-08-10 19:24:50 +0800

    init commit on script1 for develop-1

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-$(function(){
+$(document).ready(function() {
+
   $button = $('#quote-now');  // this is the button
   $content = $('#quote-content'); // this selects the result <p>
-  api_url = "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous"; // this is the url for the api
-  $button.on('click', function(){
-    $.ajax({
-      // put the necessary code here to hit the api
-    }).always(alwaysFunction)
-      .success(successFunction)
-      .fail(failFunction);
 
-  function alwaysFunction(){
-    console.log("yay!");
-    // do we need this? if not take it out
-  }
+$button.on('click', function() {
+  console.log('click click click');
+  $.ajax({
+    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous',
 
-  function successFunction(data){
-    // put the action to show the quote and the person who said the quote in the $result
-    // we can have this do some changes to the styling to (eg make the container for the result appear only when success)
-  }
+    type: 'POST',
+    data: {
+      cat: "famous"
+    },
+    // datatype: 'json',
+    success: function(data) {alert(data);
+    $('#quote-content').html(data);
+    },
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader('X-Mashape-Key', 'fKNj4SkY1lmshrYv3plHC6jXCNmdp1YPo3XjsnujdpbI1rHohn');
+    }
+    })
+    .fail(function(request, textStatus, errorThrown) {
+      $result.html(request.status + ' ' + textStatus + ' ' + errorThrown);
 
-  function failFunction(jqXHR, textStatus, errorThrown){
-    console.log(errorThrown);
-  }
-  })
-})
+    });
+  });
+
+});
