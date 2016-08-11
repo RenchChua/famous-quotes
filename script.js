0e291ff457415f7dfdc6ac8483591435df8c78d5
Sebastian Sim <phistando@yahoo.com>
2016-08-11 19:31:16 +0800

    edit script ajax

--- script.js
+++ script.js
@@ -15,10 +15,6 @@ $button.on('click', function() {
       cat: "famous"
     },
 
-    // success: function(data) {alert(data);
-    // $content.html(data);
-    // },
-
     beforeSend: function(xhr) {
       xhr.setRequestHeader('X-Mashape-Key', 'fKNj4SkY1lmshrYv3plHC6jXCNmdp1YPo3XjsnujdpbI1rHohn');
     }
@@ -37,43 +33,3 @@ $button.on('click', function() {
   conflict.concat('resolved?');
 
 });
-
-
-
-// $(function(){
-//   $button = $('#quote-now');  // this is the button
-//   $result = $('#result'); // this selects the result <p>
-//   api_url = "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous"; // this is the url for the api
-//
-//   $button.on('click', function(){
-//     $.ajax({
-//       // put the necessary code here to hit the api
-//       dataType: 'json',
-//       type: 'POST',
-//       data: {
-//         cat: "famous"
-//       },
-//       beforeSend: function(xhr) {
-//         xhr.setRequestHeader('X-Mashape-Key', 'fKNj4SkY1lmshrYv3plHC6jXCNmdp1YPo3XjsnujdpbI1rHohn');
-//       }
-//     }).success(successFunction)
-//       .fail(failFunction);
-//
-//
-//   // function alwaysFunction(){
-//   //   console.log("yay!");
-//   //   // do we need this? if not take it out
-//   // }
-//
-//   function successFunction(data){
-//     // put the action to show only the quote in the $result
-//     // we can have this do some changes to the styling to (eg make the container for the result appear only when success)
-//     alert(data);
-//     $('#quote-content').html(data);
-//   }
-//
-//   function failFunction(jqXHR, textStatus, errorThrown){
-//     console.log(errorThrown);
-//   }
-//   })
-// })
